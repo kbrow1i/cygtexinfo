8ec3b73ebfa6d853286085f142bbafc50ca5d06c
Ken Brown <kbrown@cornell.edu>
2017-04-23 17:57:29 -0400

    Enable tests

--- appveyor.sh
+++ appveyor.sh
@@ -6,7 +6,7 @@ cygport texinfo.cygport fetch prep compile || exit_status=1
 if [ $exit_status -eq 0 ]
 then
     cygport texinfo.cygport inst pkg || exit_status=1
-    # cygport texinfo.cygport test || echo "Test(s) failed."
+    cygport texinfo.cygport test || echo "Test(s) failed."
 fi
 
 tar -cJf artifact.tar.xz texinfo-*/dist texinfo-*/log
